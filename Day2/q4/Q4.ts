38a79424fc33d8c59b8ba24e59bdd719e2e5ef22
Muhammad-Aasad <m96999276@gmail.com>
2024-03-11 12:51:59 +0500

    comit

---
+++ Day2/q4/Q4.ts
@@ -0,0 +1,10 @@
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+let famous_person: string = "Albert Einstein ";
+let famousqoute: string = "A person who never made a mistake never tried anything new.";
+
+//compose 
+
+let message: string = `${famous_person} once said , ${famousqoute}`;
+console.log(message);
---
+++ Day2/q5/Q5.js
@@ -0,0 +1,5 @@
+"use strict";
+//Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. Make sure you use each character combination, "\t" and "\n", at least once. Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.
+let person_name = "\t\n ASad\t\n"; // This saves the name with whitespace
+console.log(person_name);
+console.log(person_name.trim());
