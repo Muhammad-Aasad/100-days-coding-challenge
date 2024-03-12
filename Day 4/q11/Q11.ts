2c55dbbe763f33021a6c92a04e2e573eee430ee7
Muhammad-Aasad <m96999276@gmail.com>
2024-03-12 22:58:32 +0500

    comit

---
+++ Day 4/q11/Q11.ts
@@ -0,0 +1,6 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let names: string[] = ["Asad", "saad", "Ali"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ Day 4/q12/Q12.ts
@@ -0,0 +1,6 @@
+//Greetings: Use the array from Exercise 11. Instead of just printing each person’s name, print a message to them. The message should be the same for each person, but personalized with their name.
+
+let names  : string [] = ["Asad,Saad,"];
+for (let name of names ){
+    console.log(`Hello ${name} ,Would you like to Learn some Typescript Today?`)
+}
