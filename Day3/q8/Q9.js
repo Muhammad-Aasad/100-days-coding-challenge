f1d53e66a6ff99739d117c42bcc37a8f96cab68d
Muhammad-Aasad <m96999276@gmail.com>
2024-03-12 22:25:55 +0500

    3rd comit

---
+++ Day3/q8/Q9.js
@@ -0,0 +1,3 @@
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+var Favorite_Number = 8; // This save yourfavourite number
+console.log("My favourite number is ".concat(Favorite_Number));
