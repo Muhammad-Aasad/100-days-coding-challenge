29bbf88f7cb48c6a7ffe71bc4e96f1d952a00d55
Muhammad-Aasad <m96999276@gmail.com>
2024-03-19 22:46:10 +0500

    comit

---
+++ Day 7/Q19/q19.ts
@@ -0,0 +1,8 @@
+// Use one of your programs from Exercises 14 through 18 to print a message indicating the number of people you are inviting to dinner.
+
+
+let guests: string[] = ["Albert Einstein", "Marie Curie", "Leonardo da Vinci", "Isaac Newton", "Charles Darwin"];
+console.log(`i am inviting,${guests.length}, people to dinner`);
+
+
+
